efec1962c9c94d083c0deef0df4c95b74dca0e8f
CBRESDS <billy.goodfellow@cbre.com>
2026-09-22 15:40:04 -0400

    Update chemicals.js

--- chemicals.js
+++ chemicals.js
@@ -4,5 +4,119 @@ const chemicals = [
     manufacturer: "RMR Solutions",
     category: "Cleaning / Mold & Mildew",
     file: "sds/RMR 86 SDS.pdf"
+  },
+  {
+    name: "3M Neutral Quat Disinfectant Cleaner Concentrate",
+    manufacturer: "3M",
+    category: "Disinfectant / Cleaning",
+    file: "3M Neutral Quat Disinfectant Cleaner Concentrate.pdf"
+  },
+  {
+    name: "Oster Spray Disinfectant",
+    manufacturer: "Oster",
+    category: "Disinfectant",
+    file: "A00224 OSTER SPRAY DSNF BTY 76300-102.pdf"
+  },
+  {
+    name: "AseptiCare TB+II",
+    manufacturer: "Unknown",
+    category: "Disinfectant",
+    file: "ASEPTICARE TB+II.pdf"
+  },
+  {
+    name: "Accel Five TB Wipes",
+    manufacturer: "Unknown",
+    category: "Disinfectant Wipes",
+    file: "Accel Five TB Wipes.pdf"
+  },
+  {
+    name: "Acid Toilet Bowl Cleaner",
+    manufacturer: "Unknown",
+    category: "Restroom Cleaning",
+    file: "Acid Toilet Bowl Cleaner (EPA Reg No. 8155-6-89900).pdf"
+  },
+  {
+    name: "Birex Quat Wipes",
+    manufacturer: "Unknown",
+    category: "Disinfectant Wipes",
+    file: "Birex Quat Wipes.pdf"
+  },
+  {
+    name: "Citro Shield Furniture Polish",
+    manufacturer: "Unknown",
+    category: "Furniture Care",
+    file: "CITRO SHIELD FURNITURE POLISH.pdf"
+  },
+  {
+    name: "Continuum AT3246",
+    manufacturer: "Unknown",
+    category: "Other",
+    file: "CONTINUUM AT3246.pdf"
+  },
+  {
+    name: "CorrShield NT4207",
+    manufacturer: "Unknown",
+    category: "Other",
+    file: "CORRSHIELD NT4207.pdf"
+  },
+  {
+    name: "CaviCide",
+    manufacturer: "Metrex",
+    category: "Disinfectant / Cleaning",
+    file: "CaviCide™ Desident™ CaviCide.pdf"
+  },
+  {
+    name: "CaviWipes",
+    manufacturer: "Metrex",
+    category: "Disinfectant Wipes",
+    file: "CaviWipes.pdf"
+  },
+  {
+    name: "Chevron 1540 Oil",
+    manufacturer: "Chevron",
+    category: "Lubricant / Oil",
+    file: "Chevron 1540 Oil SDS.pdf"
+  },
+  {
+    name: "Clorox Disinfecting Wipes - Fresh Scent",
+    manufacturer: "Clorox",
+    category: "Disinfectant Wipes",
+    file: "Clorox Disinfecting Wipes, Fresh Scent.pdf"
+  },
+  {
+    name: "Clorox Germicidal Bleach",
+    manufacturer: "Clorox",
+    category: "Bleach / Disinfectant",
+    file: "Clorox Germicidal Bleach.pdf"
+  },
+  {
+    name: "Clorox Healthcare Bleach Germicidal Wipes",
+    manufacturer: "Clorox",
+    category: "Disinfectant Wipes",
+    file: "Clorox Healthcare Bleach Germicidal Wipes.pdf"
+  },
+  {
+    name: "Common Salt without Additives",
+    manufacturer: "Unknown",
+    category: "Other",
+    file: "Common Salt without Additives.pdf"
+  },
+  {
+    name: "Crew Bathroom Disinfectant Cleaner",
+    manufacturer: "Diversey",
+    category: "Restroom Cleaning / Disinfectant",
+    file: "Crew® Bathroom Disinfectant Cleaner.pdf"
+  },
+  {
+    name: "Drano Liquid Drain Cleaner",
+    manufacturer: "SC Johnson",
+    category: "Drain Cleaner",
+    file: "DRANO® LIQUID DRAIN CLEANER.pdf"
+  },
+  {
+    name: "Dawn Dish Soap",
+    manufacturer: "Procter & Gamble",
+    category: "Cleaning",
+    file: "Dawn Dish Soap SDS.pdf"
   }
 ];
